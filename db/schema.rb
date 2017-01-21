beb83b1b430e9e2c7469bcb2d6a634ea0f6afb29
Terry Ramsdale <terrams@me.com>
2017-01-21 14:01:40 -0600

    update logo header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114231708) do
+ActiveRecord::Schema.define(version: 20170121042516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,11 +81,12 @@ ActiveRecord::Schema.define(version: 20170114231708) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
