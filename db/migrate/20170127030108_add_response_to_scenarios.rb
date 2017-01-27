e8682b572ef224e7641529dcb9dcf5e11732939a
Terry Ramsdale <terrams@me.com>
2017-01-26 22:47:26 -0600

    updates

--- db/migrate/20170127030108_add_response_to_scenarios.rb
+++ db/migrate/20170127030108_add_response_to_scenarios.rb
@@ -1,8 +1,8 @@
 class AddResponseToScenarios < ActiveRecord::Migration[5.0]
   def change
     add_column :scenarios, :response, :text
-    add_column :scenarios, :response, :text
-    add_column :scenarios, :response, :text
-    add_column :scenarios, :response, :text    
+    add_column :scenarios, :supplies, :text
+    add_column :scenarios, :likelyhood, :text
+    add_column :scenarios, :threat, :text    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125230346) do
+ActiveRecord::Schema.define(version: 20170127030108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,10 @@ ActiveRecord::Schema.define(version: 20170125230346) do
     t.string   "icon"
     t.string   "tag"
     t.string   "teaser"
+    t.text     "response"
+    t.text     "supplies"
+    t.text     "likelyhood"
+    t.text     "threat"
   end
 
   create_table "users", force: :cascade do |t|
